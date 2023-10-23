12e8b0bbd3bea0d8cb8b210df4e8107128cad39b
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-10-23 11:28:48 -0700

    no modulous

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,14 +1,14 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        int fizzCounter = 3;
+        int fizzCounter = 3; //keep track of # of times print ("Fizz") is executed
         int buzzCounter = 5;
         for (int i = 1; i <= 100; i++) {
-            fizzCounter--;
+            fizzCounter--; //increments down to 0
             buzzCounter--;
             if (fizzCounter == 0 && buzzCounter == 0) {
                 System.out.println("FizzBuzz");
-                fizzCounter = 3;
-                buzzCounter = 5;
+                fizzCounter = 3; //reset counter to 3
+                buzzCounter = 5; //reset counter to 5
             } else if (fizzCounter == 0) {
                 System.out.println("Fizz");
                 fizzCounter = 3;
